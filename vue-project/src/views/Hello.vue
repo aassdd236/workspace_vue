2b83723101e03af9eea8fd1ebc4a8028902db68b
aassdd236 <aassdd236@naver.com>
2024-03-20 16:23:44 +0900

    03/20

---
+++ vue-project/src/views/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <h1>Hello Vue.js</h1>
+
+</template>
+
+
+<script>
+
+    export default {
+        name: "HelloView",
+        data(){
+            return{
+
+            }
+        }
+    }
+
+</script>
+
+
+<style>
+
+</style>
